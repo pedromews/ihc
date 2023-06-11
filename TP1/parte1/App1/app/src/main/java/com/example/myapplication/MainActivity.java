85fbf1171bb66f2eebdce489b2879782be100953
Pedro Mews <pedro.mews@sap.com>
2023-06-11 18:40:27 -0300

    fixes

--- TP1/parte1/App1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ TP1/parte1/App1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         EditText num1 = findViewById(R.id.num1);
         EditText num2 = findViewById(R.id.num2);
         TextView result = findViewById(R.id.result);
-        Button add = findViewById(R.id.add_button);
+        Button add = findViewById(R.id.sum_button);
 
         add.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
