c3f534299919d6b8cc77d943fd27d5c320dc1e9c
Pedro Mews <pedro.mews@sap.com>
2023-06-11 17:03:09 -0300

    show action bar

--- TP1/parte1/App1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ TP1/parte1/App1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,23 +3,10 @@ package com.example.myapplication;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.text.Editable;
 import android.view.View;
 
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.myapplication.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
